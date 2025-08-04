d9513711b3fbe0726f0e98c497338f530620d46e
dagm <dagimyibabe19@gmail.com>
2025-08-04 20:31:29 +0300

    ternary operator

---
+++ src/ternaryOperator/ternary.java
@@ -0,0 +1,16 @@
+package ternaryOperator;
+
+public class ternary {
+    // (condition) ? True : False
+    // (condition statemnt) ? if true : if false
+    public static void main(String[] args) {
+
+        String isStudent = (false) ? "Yes" : "No";
+        System.out.println(isStudent);
+
+        boolean isincludes= (isStudent.contains("Y")) ? true : false;
+        System.out.println(isincludes);
+
+    }
+
+}

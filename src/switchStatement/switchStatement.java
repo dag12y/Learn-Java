43293a2c302bdef83de37eaeb59b183689d4548c
dagm <dagimyibabe19@gmail.com>
2025-08-04 20:45:30 +0300

    edit switch statement example

--- src/switchStatement/switchStatement.java
+++ src/switchStatement/switchStatement.java
@@ -1,20 +1,19 @@
 package switchStatement;
+import java.util.Scanner;
 
 public class switchStatement {
     public static void main(String[] args) {
-        int day = 2;
-        switch (day) {
-            case 1:
-                System.out.println("Monday");
-                break;
-            case 2:
-                System.out.println("Tuesday");
-                break;
-            case 3:
-                System.out.println("Wednesday");
-                break;
-            default:
-                System.out.println("Invalid day");
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter a Day : ");
+        String day = scanner.next();
+
+        switch (day.toLowerCase()){
+            case "monday","tuesday","wednsday","thursday","friday" ->
+                    System.out.println("it is working day");
+            case "saturday","sunday" ->
+                    System.out.println("it is weekend");
+            default -> System.out.println(day+" is not a day");
         }
+        scanner.close();
     }
 }
